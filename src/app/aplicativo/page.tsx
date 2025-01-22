778b1c865678e1e0e71199f8edaf7d33c168b9ce
kaiquPaula <kaique.paula@argoninformatica.com.br>
2025-01-22 15:38:55 -0300

    Cria estrutura inicial de rotas e páginas
    - Adiciona rotas e arquivos para as páginas: Home, Sobre, Contato, Blog e App.

---
+++ src/app/aplicativo/page.tsx
@@ -0,0 +1,7 @@
+export default function Aplicativo() {
+  return (
+    <div>
+      <h1>Página Aplicativo</h1>
+    </div>
+  );
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <div>
+      <h1>Página Blog</h1>
+    </div>
+  );
+}
