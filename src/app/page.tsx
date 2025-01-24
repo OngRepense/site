3297c6bab82acdb0addd985ed3b377a6e5193a63
KIQ <kaiqueoliveira257@gmail.com>
2025-01-24 15:26:40 -0300

    Finaliza desenvolvimento da página Home
    - Conclui a implementação do conteúdo e estilo da página inicial.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,168 @@
-import Image from "next/image";
+import { Metadata } from "next";
+import Image from 'next/image';
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Home - Repense",
+  description: "Descrição home repense",
+  openGraph: {
+    title: "Open graph title",
+    description: "Open graph descricao"
+  }
+}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <section className="bannerHome max-md:pb-16 py-32">
+        <div className="innerWidth max-md:flex-col-reverse max-md:gap-8 max-w-8xl flex items-center justify-between px-4 mx-auto">
+          <div className="conteudoBanner max-w-xl flex flex-col gap-4">
+            <h2 className="text-primary max-md:text-3xl max-md:text-center font-caveat text-6xl font-bold">Um novo jeito de encontrar oportunidades!</h2>
+            <p className="max-md:text-sm max-md:text-center text-xl">Nós da ONG Repense acreditamos que a Educação e o acesso à Informação de qualidade são fatores de extrema importância para que possamos mudar de vida.</p>
+          </div>
+          <div className="imagemBanner max-md:max-w-[210px] max-w-[600px] w-full flex items-center justify-center">
+            <Image src="/assets/icons/mascoteBanner.svg" alt="Mascote da Repense" width={430} height={600} loading="lazy" />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+      <section className="sobreHome max-md:py-16 py-32">
+        <div className="innerWidth max-md:flex-col max-md:gap-8 max-w-8xl flex items-center justify-between px-4 mx-auto">
+          <div className="sobreNumerosHome flex gap-4">
+            <div className="sobreNumero max-md:rounded-lg max-w-[332px] max-h-[332px] w-full flex flex-col items-center justify-center rounded-3xl bg-primary overflow-hidden md:p-8">
+              <h3 className="max-md:text-2xl text-center text-8xl font-bold">+5</h3>
+              <p className="max-md:text-xs text-center text-2xl">Anos transformando experiências em resultados</p>
+            </div>
+            <div className="mascoteSobre max-md:rounded-lg max-w-[332px] max-h-[332px] w-full flex items-center justify-center rounded-3xl bg-tertiary overflow-hidden">
+              <Image className="w-full h-full" src="/assets/icons/mascoteComPasta.svg" alt="Mascote da Repense" width={430} height={600} loading="lazy" />
+            </div>
+          </div>
+          <div className="sobreConteudoHome max-md:items-center max-w-xl flex flex-col gap-8">
+            <div className="max-md:items-center max-w-xl flex flex-col gap-4">
+              <h2 className="text-primary max-md:text-3xl max-md:text-center font-caveat text-6xl font-bold">Sobre nós</h2>
+              <p className="max-md:text-sm max-md:text-center text-xl">Nosso objetivo é bem simples: FAZER A DIFERENÇA NA VIDA DAS PESSOAS. Por isso decidimos constituir a Repense, uma ONG que visa capacitar pessoas, conectando pessoas.</p>
+            </div>
+            <button className="group w-fit border-2 border-primary flex items-center justify-center gap-4 text-primary font-bold rounded hover:bg-primary hover:text-tertiary duration-300 ease-in-out py-3 px-6">
+              <span>Saiba mais</span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 16 16" fill="none">
+                <path className="group-hover:fill-tertiary group-hover:stroke-tertiary" d="M4.66699 11.3337L11.3337 4.66699M11.3337 4.66699H4.66699M11.3337 4.66699V11.3337" stroke="#F07D00" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="blogHome max-md:py-16 bg-tertiary py-32">
+        <div className="innerWidth max-w-8xl flex flex-col items-center justify-center gap-8 px-4 mx-auto">
+          <div className="sobreConteudoHome max-w-3xl flex flex-col items-center gap-4">
+            <h2 className="text-primary max-md:text-3xl max-md:text-center font-caveat text-6xl text-secondary font-bold">Confira nosso blog</h2>
+            <p className="max-md:text-sm text-center text-xl text-secondary">Explore conteúdos inspiradores e dicas práticas para transformar sua vida profissional. Descubra histórias, tendências e muito mais!</p>
+          </div>
+          <button className="w-fit bg-primary flex items-center justify-center gap-4 rounded hover:bg-opacity-85 duration-300 ease-in-out py-3 px-6">
+            <span>Saiba mais</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 16 16" fill="none">
+              <path d="M4.66699 11.3337L11.3337 4.66699M11.3337 4.66699H4.66699M11.3337 4.66699V11.3337" stroke="#FFF1C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </button>
+        </div>
+      </section>
+      <section className="oqueFazemosHome max-md:py-16 py-32">
+        <div className="innerWidth max-w-8xl flex flex-col items-center justify-center gap-16 px-4 mx-auto">
+          <h2 className="text-tertiary max-md:text-3xl max-md:text-center font-caveat text-6xl  font-bold">O que fazemos?</h2>
+          <div className="projetosHome w-full flex flex-col gap-8">
+            <div className="projeto w-full flex max-md:flex-col md:gap-8">
+              <div className="projetoImagem max-md:rounded-t-lg w-full md:rounded-3xl overflow-hidden bg-custom-gradient-primary-02 md:bg-custom-gradient-primary">
+                <Image className="min-h-[132px] w-full h-full object-cover relative z-[-1]" src="/assets/images/bancaComunitariaBanner.png" alt="Mascote da Repense" width={974} height={344} loading="lazy" />
+              </div>
+              <div className="projetoCard max-md:rounded-b-lg md:max-w-[384px] w-full flex flex-col gap-8 md:rounded-3xl bg-primary overflow-hidden max-md:p-4 p-8">
+                <div className="flex flex-col gap-4">
+                  <h3 className="max-md:text-2xl text-3xl font-bold font-caveat">Inscreva-se para a nossa banca comunitária!</h3>
+                  <p className="max-md:text-xs text-base">Oferecemos um espaço acolhedor onde as crianças podem passar o tempo com atividades educativas, recreativas e de integração social. Um ambiente seguro e divertido para aprender e crescer.</p>
+                </div>
+                <Link href="#" className="flex items-center gap-2">
+                  <span className="font-medium">Saiba mais</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 16 16" fill="none">
+                    <path d="M4.66699 11.3337L11.3337 4.66699M11.3337 4.66699H4.66699M11.3337 4.66699V11.3337" stroke="#FFF1C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  </svg>  
+                </Link>
+              </div>
+            </div>
+            <div className="projeto w-full flex max-md:flex-col-reverse md:gap-8">
+              <div className="projetoCard max-md:rounded-b-lg md:max-w-[384px] w-full flex flex-col gap-8 md:rounded-3xl bg-tertiary overflow-hidden max-md:p-4 p-8">
+                <div className="flex flex-col gap-4">
+                  <h3 className="max-md:text-2xl text-3xl font-bold font-caveat text-secondary">Vagas de emprego</h3>
+                  <p className="max-md:text-xs text-base text-secondary">Acesse diversas oportunidades de trabalho em um só lugar. Estamos conectando você às vagas mais recentes para facilitar sua busca por emprego.</p>
+                </div>
+                <Link href="#" className="flex items-center gap-2">
+                  <span className="font-medium text-secondary">Saiba mais</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 16 16" fill="none">
+                    <path d="M4.66699 11.3337L11.3337 4.66699M11.3337 4.66699H4.66699M11.3337 4.66699V11.3337" stroke="#234B64" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  </svg>  
+                </Link>
+              </div>
+              <div className="projetoImagem max-md:rounded-t-lg w-full md:rounded-3xl overflow-hidden bg-custom-gradient-tertiary-02 md:bg-custom-gradient-tertiary">
+                <Image className="min-h-[132px] w-full h-full object-cover relative z-[-1]" src="/assets/images/vagasBanner.png" alt="Mascote da Repense" width={974} height={344} loading="lazy" />
+              </div>
+            </div>
+            <div className="projeto w-full flex max-md:flex-col md:gap-8">
+              <div className="projetoImagem max-md:rounded-t-lg w-full md:rounded-3xl overflow-hidden bg-custom-gradient-quaternary-02 md:bg-custom-gradient-quaternary">
+                <Image className="min-h-[132px] w-full h-full object-cover relative z-[-1]" src="/assets/images/geradorCurriculoBanner.png" alt="Mascote da Repense" width={974} height={344} loading="lazy" />
+              </div>
+              <div className="projetoCard max-md:rounded-b-lg md:max-w-[384px] w-full flex flex-col gap-8 md:rounded-3xl bg-quaternary overflow-hidden max-md:p-4 p-8">
+                <div className="flex flex-col gap-4">
+                  <h3 className="max-md:text-2xl text-3xl font-bold font-caveat">Gerador de curriculo Repense</h3>
+                  <p className="max-md:text-xs text-base">Tenha tudo na palma da sua mão! Acompanhe vagas de emprego, crie seu currículo profissional e fique por dentro de dicas e novidades com o nosso blog.</p>
+                </div>
+                <Link href="#" className="flex items-center gap-2">
+                  <span className="font-medium">Saiba mais</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 16 16" fill="none">
+                    <path d="M4.66699 11.3337L11.3337 4.66699M11.3337 4.66699H4.66699M11.3337 4.66699V11.3337" stroke="#FFF1C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  </svg>  
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="depoimentosHome py-16">
+        <div className="innerWidth max-w-8xl flex flex-col items-center justify-center gap-16 px-4 mx-auto">
+          <div className="depoimentoContainer w-full bg-tertiary flex flex-col items-center justify-center gap-12 rounded-3xl max-md:gap-8 max-md:p-8 p-16">
+            <p className="max-md:text-2xl font-caveat text-secondary text-3xl leading-[1.25]">“Como fundador da ONG Repense, me sinto bastante orgulhoso em trabalhar nesse projeto, pelos desafios superados para poder chegar até aqui, muito orgulho pelo projeto em si, que visa contribuir de forma positiva nas vidas das pessoas. Mas realmente acredito que os maiores beneficiados somos nós, que trabalhamos nele ou em qualquer coisa que visa o próximo como beneficiário. Para mim como pessoa foi e está sendo uma experiência maravilhosa, mais como profissional posso afirmar que me tornei um novo profissional. Ter que lidar não somente com o escopo técnico mas também com o insumo principal que são as pessoas.”</p>
+            <div className="depoimentoDetalhes w-full flex items-center justify-center gap-4">
+              <Image className="max-w-[64px] max-h-[64px] w-full h-full object-cover border-[5px] border-[rgba(36,75,100,0.32)] rounded-full" src="/assets/images/dmeval.png" alt="Dmeval Neto - Presidente - Ong Repense" width={56} height={56} loading="lazy" />
+              <div className="depoimentoPessoa flex flex-col">
+                <p className="nomePessoa text-secondary font-bold max-md:text-base text-lg">Dmeval Neto</p>
+                <p className="cargo text-secondary max-md:text-sm text-base">Presidente - Ong Repense</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="parceriasHome max-md:py-16 py-32">
+        <div className="innerWidth max-w-8xl flex flex-col items-center justify-center gap-16 px-4 mx-auto">
+          <h2 className="text-tertiary max-md:text-3xl max-md:text-center font-caveat text-6xl font-bold">Parcerias que nos orgulhamos</h2>
+          <div className="parceriasContainer w-full flex max-md:gap-4 gap-8">
+            <div className="parceriaItem w-full bg-[rgba(240,125,0,0.50)] flex items-center justify-center max-md:rounded-lg md:rounded-3xl py-20 px-8">
+              <Image className="max-w-[100px] max-h-[100px] w-full" src="/assets/images/googleIcon.png" alt="Parceria" width={100} height={100} loading="lazy" />
+            </div>
+            <div className="parceriaItem w-full bg-[rgba(240,125,0,0.50)] flex items-center justify-center max-md:rounded-lg md:rounded-3xl py-20 px-8">
+              <Image className="max-w-[100px] max-h-[100px] w-full" src="/assets/images/itauLogo.png" alt="Parceria" width={100} height={100} loading="lazy" />
+            </div>
+            <div className="parceriaItem w-full bg-[rgba(240,125,0,0.50)] flex items-center justify-center max-md:rounded-lg md:rounded-3xl py-20 px-8">
+              <Image className="max-w-[100px] max-h-[100px] w-full" src="/assets/images/santanderLogo.png" alt="Parceria" width={100} height={100} loading="lazy" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="contatoHome py-16">
+        <div className="innerWidth max-w-8xl flex flex-col items-center justify-center gap-16 px-4 mx-auto">
+          <h2 className="text-tertiary max-md:text-3xl max-md:text-center font-caveat text-6xl font-bold">Que tal falar conosco?</h2>
+          <button className="w-fit bg-primary flex items-center justify-center gap-4 rounded hover:bg-opacity-85 duration-300 ease-in-out py-3 px-6">
+            <span>Saiba mais</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 16 16" fill="none">
+              <path d="M4.66699 11.3337L11.3337 4.66699M11.3337 4.66699H4.66699M11.3337 4.66699V11.3337" stroke="#FFF1C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </button>
+        </div>
+      </section>
     </div>
   );
 }
