a85d15aa4337a1acc751997d75c1096b9b43a852
KIQ <kaiqueoliveira257@gmail.com>
2025-01-24 15:26:01 -0300

    Adiciona novas cores e gradientes no Tailwind CSS
    - Configura novas cores e gradientes personalizados no arquivo `tailwind.config.ts`.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         primary: "#F07D00",
         secondary: "#234B64",
         tertiary: "#FFF1C3",
+        quaternary: "#00696E",
+        softBlue: "#295774",
+        borderBlue: "#31607D",
       },
       fontFamily: {
         nunito: ['"Nunito"', "sans-serif"],
@@ -19,6 +22,14 @@ export default {
       },
       maxWidth: {
         '8xl': '86.875rem',
+      },
+      backgroundImage: {
+        'custom-gradient-primary': "linear-gradient(180deg, rgba(240, 125, 0, 0.72) 0%, rgba(240, 125, 0, 0) 100%)",
+        'custom-gradient-primary-02': "linear-gradient(180deg, rgba(240, 125, 0, 0) 0%, rgba(240, 125, 0, 1)  100%)",
+        'custom-gradient-tertiary': "linear-gradient(180deg, rgba(255, 241, 195, 0.72) 0%, rgba(240, 125, 0, 0) 100%)",
+        'custom-gradient-tertiary-02': "linear-gradient(180deg, rgba(240, 125, 0, 0) 0%, rgba(255, 241, 195, 1)  100%)",
+        'custom-gradient-quaternary': "linear-gradient(180deg, rgba(0, 105, 110, 0.72) 0%, rgba(240, 125, 0, 0) 100%)",
+        'custom-gradient-quaternary-02': "linear-gradient(180deg, rgba(240, 125, 0, 0) 0%, rgba(0, 105, 110, 1)  100%)",
       },
     },
   },
