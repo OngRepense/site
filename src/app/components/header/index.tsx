0116a0ffcf63f01230b6eb66a0e811b1dab193eb
KIQ <kaiqueoliveira257@gmail.com>
2025-01-27 16:33:43 -0300

    Melhora responsividade do Header
    - Ajusta estilos para melhor exibição em dispositivos móveis e tablets.

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -18,14 +18,14 @@ export function Header() {
                 <Link href="/" className="max-w-[132px]">
                     <Image src="/assets/icons/repenseLogo.svg" alt="Logo Repense" width={222} height={50} loading="lazy" />
                 </Link>
-                <div className="flex md:order-2 gap-4">
-                    <button className="bg-primary flex items-center gap-4 text-tertiary rounded hover:bg-opacity-85 py-3 px-6">
-                        <span>Login</span>
+                <div className="flex md:order-2 gap-2 xs:gap-4">
+                    <button className="bg-primary flex items-center gap-4 text-tertiary  rounded hover:bg-opacity-85 py-1 px-4 sm:py-3 sm:px-6">
+                        <span className="text-xs xs:text-sm sm:text-base">Login</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 16 16">
                             <path d="M4.667 11.334L11.334 4.667M11.334 4.667H4.667M11.334 4.667v6.667" stroke="#FFF1C3" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
                     </button>
-                    <button data-collapse-toggle="navbar-sticky" type="button" className="group md:hidden p-3 text-tertiary rounded-lg hover:bg-softBlue">
+                    <button data-collapse-toggle="navbar-sticky" type="button" className="group md:hidden p-2 xs:p-3 text-tertiary rounded-lg hover:bg-softBlue">
                         <span className="sr-only">Open menu</span>
                         <svg className="w-5 h-5" fill="none" viewBox="0 0 17 14">
                             <path className="group-hover:fill-tertiary" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
