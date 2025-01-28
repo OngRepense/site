9e2e2d0fb8e8aac301bf34bf6f23e3f146d949d1
KIQ <kaiqueoliveira257@gmail.com>
2025-01-28 01:24:07 -0300

    Adiciona Footer ao layout principal
    - Inclui o componente Footer em `layout.tsx` para exibição em todas as páginas.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Header } from "@components/header";
+import { Footer } from "@components/footer";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -62,6 +63,7 @@ export default function RootLayout({
       <body className="antialiased">
         <Header />
         {children}
+        <Footer/>
       </body>
     </html>
   );
