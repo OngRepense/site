5548e6eb67682bfcd8c8c6160ce51eb468e8537c
KIQ <kaiqueoliveira257@gmail.com>
2025-02-04 21:27:45 -0300

    Adiciona google como parceiro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -150,13 +150,17 @@ export default function Home() {
               ]}
               className='max-md:rounded-lg md:rounded-3xl overflow-hidden'
             >
-              <CarouselContent className='min-h-[260px] justify-center'>
+               <CarouselContent className='md:flex md:justify-center'>
+                <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                  <div className="parceriaItem w-full bg-[rgba(240,125,0,0.50)] flex items-center justify-center max-md:rounded-lg md:rounded-3xl py-20 px-8">
+                    <Image className="max-w-[100px] max-h-[100px] w-full" src="/assets/images/googleIcon.png" alt="Parceria" width={100} height={100} loading="lazy" />
+                  </div>
+                </CarouselItem>
                 <CarouselItem className="md:basis-1/2 lg:basis-1/3">
                   <div className="parceriaItem w-full bg-[rgba(240,125,0,0.50)] max-h-[260px] h-full flex items-center justify-center max-md:rounded-lg md:rounded-3xl py-20 px-8">
                     <Image className="max-h-[72px] w-full h-full" src="/assets/icons/fiapLogo.svg" alt="Parceria" width={100} height={100} loading="lazy" />
                   </div>
                 </CarouselItem>
-                
               </CarouselContent>
             </Carousel>
           </div>
